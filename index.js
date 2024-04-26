7a3c2d3619b66cb9e27956d37a5c3a48a8a02ba1
rakib <rakib135836@gmail.com>
2024-04-26 20:24:23 +0600

    setuping server

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const app=express();
+const port=process.env.PORT||5000;
+
+// middleWire
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('server is runnig')
+  })
+  
+  app.listen(port, () => {
+    console.log(`server is running in port: ${port}`)
+  })
