e6972ac64d7e2911e365242606822313f0edf4d6
rakib <rakib135836@gmail.com>
2024-04-27 20:51:55 +0600

    env fixed

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ app.use(express.json());
 
 
 
-// const uri = "mongodb+srv://rakib224127:H39wjXPIH586OTHO@cluster0.qtepxet.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qtepxet.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qtepxet.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(uri)
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
