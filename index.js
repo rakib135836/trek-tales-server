5e6061636bdd0185a17a791ce02587297f21c92c
rakib <rakib135836@gmail.com>
2024-04-28 20:10:30 +0600

    read operation done

--- index.js
+++ index.js
@@ -41,9 +41,20 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const spotCollection=client.db('spotDB').collection('spot');
+
+
+    app.get('/new-spot',async(req,res)=>{
+      const cursor=spotCollection.find();
+      const result=await cursor.toArray();
+      res.send(result);
+    })
+
     app.post('/new-spot',async(req,res)=>{
-     const newSpot=req.body;
-     console.log(newSpot);
+     const newTouristSpot=req.body;
+     console.log(newTouristSpot);
+     const result=await spotCollection.insertOne(newTouristSpot);
+     res.send(result);
     })
 
 
